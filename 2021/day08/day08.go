18be93ecf52301c28170acfc17f617e6b52126ee
Aaron Davis <adavis100@gmail.com>
2021-12-19 06:48:23 -0800

    Add day 8 part 2

--- 2021/day08/day08.go
+++ 2021/day08/day08.go
@@ -6,22 +6,27 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-func countNums(r io.Reader) int {
+func countNums(r io.Reader, onlyUnique bool) int {
 	scanner := bufio.NewScanner(r)
 	count := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		count += countNumsInLine(line)
+		if onlyUnique {
+			count += countUniqueNumsInLine(line)
+		} else {
+			count += lineToNum(line)
+		}
 	}
 	return count
 }
 
-func countNumsInLine(s string) int {
-	parts := strings.Split(s, " | ")
-	digits := strings.Split(parts[1], " ")
+func countUniqueNumsInLine(s string) int {
+	_, digits := parseLine(s)
 	count := 0
 	for _, s := range digits {
 		if isUnique(len(s)) {
@@ -35,6 +40,100 @@ func isUnique(length int) bool {
 	return length == 2 || length == 3 || length == 4 || length == 7
 }
 
+func lineToNum(l string) int {
+	signals, digits := parseLine(l)
+	nums := deduceNums(signals)
+
+	m := make(map[string]string, 10)
+	for i := 0; i < 10; i++ {
+		m[sortStr(nums[i])] = strconv.Itoa(i)
+	}
+	numStr := ""
+	for _, digit := range digits {
+		numStr += m[sortStr(digit)]
+	}
+	n, err := strconv.Atoi(numStr)
+	if err != nil {
+		log.Println(err)
+	}
+	if n == 470 {
+		fmt.Println("here")
+	}
+	fmt.Println(n)
+	return n
+}
+
+func sortStr(s string) string {
+	r := []rune(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
+}
+
+func deduceNums(signals []string) []string {
+	nums := make([]string, 10)
+	count := 0
+	for count < 10 {
+		for _, signal := range signals {
+			if len(signal) == 2 && nums[1] == "" {
+				nums[1] = signal
+				count++
+			} else if len(signal) == 3 && nums[7] == "" {
+				nums[7] = signal
+				count++
+			} else if len(signal) == 4 && nums[4] == "" {
+				nums[4] = signal
+				count++
+			} else if len(signal) == 7 && nums[8] == "" {
+				nums[8] = signal
+				count++
+			} else if len(signal) == 5 {
+				if nums[1] != "" && diffChars(signal, nums[1]) == 0 && nums[3] == "" {
+					nums[3] = signal
+					count++
+				} else if nums[4] != "" && diffChars(signal, nums[4]) == 1 && nums[5] == "" && nums[3] != "" && signal != nums[3] {
+					nums[5] = signal
+					count++
+				} else if nums[4] != "" && diffChars(signal, nums[4]) == 2 && nums[2] == "" {
+					nums[2] = signal
+					count++
+				}
+			} else if len(signal) == 6 {
+				if nums[1] != "" && diffChars(signal, nums[1]) == 1 && nums[6] == "" {
+					nums[6] = signal
+					count++
+				} else if nums[4] != "" && diffChars(signal, nums[4]) == 0 && nums[9] == "" {
+					nums[9] = signal
+					count++
+				} else if nums[4] != "" && diffChars(signal, nums[4]) == 1 && nums[0] == "" && nums[6] != "" && signal != nums[6] {
+					nums[0] = signal
+					count++
+				}
+			}
+		}
+	}
+
+	return nums
+}
+
+func diffChars(s string, chars string) int {
+	n := 0
+	for _, c := range chars {
+		if !strings.Contains(s, string(c)) {
+			n++
+		}
+	}
+	return n
+}
+
+func parseLine(l string) (signals []string, digits []string) {
+	parts := strings.Split(l, " | ")
+	signals = strings.Split(parts[0], " ")
+	digits = strings.Split(parts[1], " ")
+	return signals, digits
+}
+
 func main() {
 	file, err := os.Open("./2021/day08/input.txt")
 	if err != nil {
@@ -42,5 +141,7 @@ func main() {
 	}
 	defer file.Close()
 
-	fmt.Println(countNums(file))
+	fmt.Println(countNums(file, true))
+	file.Seek(0, io.SeekStart)
+	fmt.Println(countNums(file, false))
 }
--- 2021/day08/day08_test.go
+++ 2021/day08/day08_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestCountsNumsInLine(t *testing.T) {
-	n := countNumsInLine("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe")
+
+	n := countUniqueNumsInLine("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe")
 	assert.Equal(t, 2, n)
 }
 
-func TestSolvesExample1(t *testing.T) {
-	lines := `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+var in = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
 fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
@@ -22,5 +22,37 @@ dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbc
 bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`
-	assert.Equal(t, 26, countNums(strings.NewReader(lines)))
+
+func TestSolvesExample1(t *testing.T) {
+	assert.Equal(t, 26, countNums(strings.NewReader(in), true))
+}
+
+func TestGetsNumsInLine(t *testing.T) {
+	tests := []struct {
+		name string
+		line string
+		want int
+	}{
+		{"1", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | ab", 1},
+		{"7", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | dab", 7},
+		{"4", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | eafb", 4},
+		{"8", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | acedgfb", 8},
+		{"3", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | fbcad", 3},
+		{"5", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfbe", 5},
+		{"2", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | gcdfa", 2},
+		{"6", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfgeb", 6},
+		{"9", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cefabd", 9},
+		{"0", "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cagedb", 0},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			n := lineToNum(test.line)
+			assert.Equal(t, test.want, n)
+		})
+	}
+}
+
+func TestSolvesExample2(t *testing.T) {
+
+	assert.Equal(t, 61229, countNums(strings.NewReader(in), false))
 }
