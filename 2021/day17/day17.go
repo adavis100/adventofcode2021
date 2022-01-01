21ef6084a7f2446ef32c94072d4c69b94f54efc5
Aaron Davis <adavis100@gmail.com>
2022-01-01 07:15:25 -0800

    Add day 17 part 2

--- 2021/day17/day17.go
+++ 2021/day17/day17.go
@@ -26,18 +26,7 @@ func main() {
 }
 
 func Solve1(r io.Reader) int {
-	buf, err := ioutil.ReadAll(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-	t := target{}
-	n, err := fmt.Sscanf(string(buf), "target area: x=%d..%d, y=%d..%d", &t.x1, &t.x2, &t.y1, &t.y2)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if n != 4 {
-		log.Fatal("Unable to parse input")
-	}
+	t := parseInput(r)
 
 	max := 0
 	for vx := 0; vx < t.x2; vx++ {
@@ -52,6 +41,22 @@ func Solve1(r io.Reader) int {
 	return max
 }
 
+func parseInput(r io.Reader) target {
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := target{}
+	n, err := fmt.Sscanf(string(buf), "target area: x=%d..%d, y=%d..%d", &t.x1, &t.x2, &t.y1, &t.y2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if n != 4 {
+		log.Fatal("Unable to parse input")
+	}
+	return t
+}
+
 func findMax(vx int, vy int, t target) int {
 	max := 0
 	x, y := 0, 0
@@ -59,7 +64,7 @@ func findMax(vx int, vy int, t target) int {
 		if inTarget(x, y, t) {
 			return max
 		} else if invalid(x, y, t) {
-			return 0
+			return -1
 		}
 		if y > max {
 			max = y
@@ -73,7 +78,7 @@ func findMax(vx int, vy int, t target) int {
 		y += vy
 		vy--
 	}
-	return 0
+	return -1
 }
 
 func inTarget(x int, y int, t target) bool {
@@ -81,10 +86,21 @@ func inTarget(x int, y int, t target) bool {
 }
 
 func invalid(x int, y int, t target) bool {
-	return x > t.x2 || y < t.y2
+	return x > t.x2 || y < t.y1
 }
 
 func Solve2(r io.Reader) int {
-	fmt.Println("Unimplemented")
-	return 0
+	t := parseInput(r)
+
+	c := 0
+	for vx := 0; vx <= t.x2; vx++ {
+		for vy := t.y1; vy < -t.y1; vy++ {
+			thisMax := findMax(vx, vy, t)
+			if thisMax >= 0 {
+				c++
+			}
+		}
+	}
+
+	return c
 }
--- 2021/day17/day17_test.go
+++ 2021/day17/day17_test.go
@@ -11,3 +11,7 @@ var ex = "target area: x=20..30, y=-10..-5"
 func TestSolvesExample(t *testing.T) {
 	assert.Equal(t, 45, Solve1(strings.NewReader(ex)))
 }
+
+func TestSolvesExample2(t *testing.T) {
+	assert.Equal(t, 112, Solve2(strings.NewReader(ex)))
+}
